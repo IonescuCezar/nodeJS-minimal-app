cd9ff33fa9a1f0132ae8aa89cc621e32dc7abb2e
Cezar Ionescu <cezar.ionescu@3pillarglobal.com>
2021-03-21 22:26:07 +0200

    + Changed form to actually send data + Added a route to deal with a new blog creation

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })) // deal with the (incoming) data (object) in the body of the request
 app.use(morgan('dev'));
 
 
@@ -39,6 +40,18 @@ app.get('/blogs', (req, res) => {
       console.log(err);
     });
 });
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
