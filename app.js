879cd5817fcb916f39f78f8fb1d50e6b6f99406c
Cezar Ionescu <cezar.ionescu@3pillarglobal.com>
2021-03-21 21:46:01 +0200

    + Added entries in mongoDB + Connected to mongoDB with mongoose + Added sandbox routes to operate on mongoDB + Added Schema and Model for blog entity

--- app.js
+++ app.js
@@ -1,18 +1,61 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://<user_id>:<password>@cluster0.ox1bw.mongodb.net/cluster0?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+  
 // register app engine
 app.set('view engine', 'ejs');
-
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+// mongoose and mongo sandboxe routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog 2',
+    snippet: 'about my new blog',
+    body: 'more about my blog'
+  });
+
+  blog.save()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('605761637036cf4016232b44')
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 
 app.get('/', (req, res) => {
   const blogs = [
